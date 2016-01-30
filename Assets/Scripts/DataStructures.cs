bb77470b76a2ca1f7aec15edb06f1a508fd821c1
Preston Hale <slethik@users.noreply.github.com>
2016-01-30 10:35:20 -0500

    Afflict victim with random affliction complete.

---
+++ Assets/Scripts/DataStructures.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataStructures : MonoBehaviour {
+
+	public static List<Affliction> possibleAfflictions;
+
+	void Start(){
+		possibleAfflictions = new List<Affliction>();
+
+		Affliction missingLimb = new Affliction("Missing Limb");
+		Affliction Frozen = new Affliction("Frozen");
+		Affliction Posessed = new Affliction("Possessed");
+		Affliction Parasite = new Affliction("Parasite");
+		Affliction zombified = new Affliction("Zombified");
+
+		Debug.Log ("Possible Afflictions: ");
+		for (int i=0; i<possibleAfflictions.Count-1; i++){
+			Debug.Log(possibleAfflictions[i].name);
+		}
+	}
+
+	public class Affliction {
+
+		public int id;
+		public string name;
+
+		public Affliction (string _name){
+			this.name = _name;
+			possibleAfflictions.Add(this);
+		}
+	}
+}
---
+++ Assets/Scripts/Victim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Victim : MonoBehaviour {
+
+	public DataStructures.Affliction affliction;
+
+	// Use this for initialization
+	void Start () {
+		// Apply a random condition to victim
+		this.affliction = generateAfflictions();
+		Debug.Log("I'm afflicted with: " + affliction.name);
+
+		// Move to the VictimPosition marker 
+		GetComponent<Transform>().position = transform.parent.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public DataStructures.Affliction generateAfflictions () {
+		List<DataStructures.Affliction> possibleAfflictions = DataStructures.possibleAfflictions;
+		int afflictionIndex = Random.Range (0, possibleAfflictions.Count - 1);
+		Debug.Log ("My affliction index is: " + afflictionIndex);
+		return possibleAfflictions [afflictionIndex];
+	} 
+}
