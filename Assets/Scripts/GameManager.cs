74ab332adf255706753d444684297ad9ac6bbac1
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2016-01-29 23:10:58 -0500

    click and drag objects done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject adventurer;
+	Transform interactionPlane;
+
+	GameObject heldObject;
+
+	// Use this for initialization
+	void Start () {
+		//make sure there is a valid adventurer on the altar
+		if (adventurer.tag == "Adventurer"){
+			Debug.Log("You have an adventurer on the altar");
+			//if (adventurer.
+			foreach (Transform child in adventurer.transform){
+				if(child.tag == "InteractionPlane"){
+					interactionPlane = child;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//getting the currently held object from the MouseControl Script
+		heldObject = GetComponent<MouseControl>().heldObject;
+		//if (heldObject  INTERACTS 
+	}
+}
