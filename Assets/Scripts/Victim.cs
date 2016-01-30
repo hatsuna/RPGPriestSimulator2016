89a9c2ca0d07f1656559657335e2c33b75765125
Preston Hale <slethik@users.noreply.github.com>
2016-01-30 13:04:00 -0500

    Sprite Render
    Renders default sprite on table.

--- Assets/Scripts/Victim.cs
+++ Assets/Scripts/Victim.cs
@@ -6,9 +6,14 @@ public class Victim : MonoBehaviour {
 
 	public DataStructures.Affliction affliction;
 	public int treatmentState;
+	public Sprite testSprite;
+
 
 	// Use this for initialization
 	void Start () {
+		// Apply sprite
+		gameObject.GetComponent<SpriteRenderer>().sprite=testSprite;
+
 		// Apply a random condition to victim
 		this.affliction = GenerateAfflictions();
 		ApplyAffliction (affliction);
@@ -30,7 +35,7 @@ public class Victim : MonoBehaviour {
 	public void ApplyAffliction (DataStructures.Affliction affliction) {
 		// Name matching is ugly, consider switching to id
 		if (affliction.name == "Frozen") {
-			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			gameObject.GetComponent<SpriteRenderer>().material.color = Color.blue;
 		} else if (affliction.name == "Possessed") {
 			gameObject.GetComponent<Renderer>().material.color = Color.magenta;
 		} else if (affliction.name == "Parasite") {
